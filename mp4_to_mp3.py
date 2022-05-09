2059f5b54b9eff97b94d452520375577fe9432c9
doonay <dementiado@gmail.com>
2022-05-09 10:20:40 +0300

    Add files via upload
    Исправил имя файла на полный путь к нему

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -8,15 +8,23 @@ def convert_video_to_audio_ffmpeg(video_file, output_ext="mp3"):
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.STDOUT)
 
+def convert_video_to_audio_ffmpeg1(video_file, output_name, output_ext="mp3"):
+    filename, ext = os.path.splitext(video_file)
+    print(filename)
+    print(ext)
+    subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"], 
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT)
+
 def get_files_from_dir(dir):
 	filelist = []
 	for filename in os.listdir(dir):
-		filelist.append(filename)
+		filelist.append(dir + '\\' + filename)
 	return(filelist)
 
 if __name__ == "__main__":
 	filelist = get_files_from_dir(sys.argv[1])
-	print(filelist)
 	for file in filelist:
-		#vf = sys.argv[1]
-		convert_video_to_audio_ffmpeg(file)
+		x = file.rfind(' ')
+		#print(file[x + 1:])
+		convert_video_to_audio_ffmpeg1(file, file[x + 1:], 'mp3')
